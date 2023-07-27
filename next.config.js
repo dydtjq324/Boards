1f833d8a75ef4e28e9653002e2eef14952729273
Lee yong sub <dydtjq324@naver.com>
2023-07-27 21:00:11 +0900

    분기배포

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true
+  trailingSlash: true,
+  generateBuildId: () => "ymarket-deploy"
 }
 
 module.exports = nextConfig
